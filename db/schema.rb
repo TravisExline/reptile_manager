0dca7f4d5a0435c875ba11f67c421abef46257f9
Travis Exline <t.exline22@gmail.com>
2019-07-11 18:40:25 +0000

    fixed shcema

--- db/schema.rb
+++ db/schema.rb
@@ -28,10 +28,11 @@ ActiveRecord::Schema.define(version: 2019_07_10_135418) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
+    t.string "username"
     t.string "email"
-    t.integer "age"
     t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
